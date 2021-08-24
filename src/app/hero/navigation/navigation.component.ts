6f0fad3eb5249d8357916e4fe32ae826527c3770
Isaac Hunter <ikehunter5@gmail.com>
2021-08-24 12:01:15 -0400

    add functionality to nav

--- src/app/hero/navigation/navigation.component.ts
+++ src/app/hero/navigation/navigation.component.ts
@@ -12,4 +12,8 @@ export class NavigationComponent implements OnInit {
   ngOnInit() {
   }
 
+  uncheck() {
+    (document.getElementById('navi-toggle') as HTMLInputElement).checked = false;
+  }
+
 }
